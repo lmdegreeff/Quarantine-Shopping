7b0b6da0a8aa24f825d86eb8edca5ed8fc4cd7a3
jdai1 <44037521+jdai1@users.noreply.github.com>
2020-05-03 14:36:05 -0400

    Update DataProcessing.py

--- DataProcessing.py
+++ DataProcessing.py
@@ -9,18 +9,26 @@ dfr = dfr[dfr['Establishment Type'].isin(options)]
 print(dfr.head())
 
 dfp = pd.read_csv('/Users/xu/Desktop/Food_Pantry_List.csv', delimiter = ',')
-dfp = dfp[['Agency Name', 'Hours of Operation', 'Location']]
+df_info = dfp[['Phone Number', 'Hours of Operation']]
+dfp = dfp[['Agency Name', 'Location']]
 dfp = dfp.applymap(lambda x: str(x).strip())
 dfp = dfp[dfp['Location'].notnull()]
 #print(dfp.head())
 
 names = dfr['Entity Name'].tolist()
 names = names + dfp['Agency Name'].tolist()
-print(len(names))
+print(names)
 
 locations = []
 for location in dfr['Location'].tolist():
     locations.append(location[location.rfind('('):])
 for location in dfp['Location'].tolist():
     locations.append(location[location.rfind('('):])
-print(len(locations))
+#print(locations)
+
+info = []
+for location in dfr['Location'].tolist():
+    info.append('no information available')
+for index, row in df_info.iterrows():
+    info.append(str(row['Phone Number']) + ' ' + row['Hours of Operation'])
+#print(info)
